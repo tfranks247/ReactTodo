d9b72db1d9870323e6622936bbfa7deaea039aca
Tony Franks <tonyfranks@Tonys-MacBook-Pro.local>
2019-11-09 00:46:27 -0500

    first react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import uuid from "uuid/v1"
 
 function App() {
+  const [todos, setTodos] = useState([
+    { name: "one", id: uuid()}, 
+    { name: "two", id: uuid()}, 
+    { name: "three", id: uuid() }
+]);
+  const [newTodo, setNewTodo] = useState ("");
+
+  const handleSubmit = event => {
+    event.preventDefault(); // the page wont refresh because of this
+    setTodos([...todos, {name: newTodo, id: uuid()}]);
+    console.log(todos);// destructuring and then adding a new todo and creating a new array
+    setNewTodo("");
+  };
+
+  const handleChange = event => {
+    setNewTodo(event.target.value);
+  };
+  const deleteTodo = id => {
+    setTodos(todos.filter(i => i.id !== id))
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {todos.map(i => (
+        <li key={i.id} onClick={() =>deleteTodo(i.id)}>
+          <b>{i.name}</b>
+          </li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+          <input type = "text" onChange = {handleChange} value= {newTodo} />
+          <input type = "submit" />
+      </form>
     </div>
+    
   );
-}
+        }
 
 export default App;
---
+++ test.js
@@ -0,0 +1,15 @@
+function printMe(){
+    console.log(someParam);
+
+    return 5;
+}
+const newPrintMe = (someParam) => {
+    console.log (someParam);
+    return 5;
+};
+
+
+let x = printMe();
+ley y = newPrintMe
+console.log(x + 6);
+console.log(x + 6);
